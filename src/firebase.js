b9bed4a0d3e4236bc4ea82245146c325e753d014
roboblox <schulerjohnthomas@gmail.com>
2018-08-05 16:49:09 -0700

    newstart

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,6 @@ import firebase from "firebase/app";
 import "firebase/app";
 import "firebase/database";
 
-const config = {
-  apiKey: "AIzaSyBpVfJFhQBscSN60hURN7PJxH9__TZEbRM",
-  authDomain: "m-city-4ca97.firebaseapp.com",
-  databaseURL: "https://m-city-4ca97.firebaseio.com",
-  projectId: "m-city-4ca97",
-  storageBucket: "m-city-4ca97.appspot.com",
-  messagingSenderId: "585461711153"
-};
-
 firebase.initializeApp(config);
 
 const firebaseDB = firebase.database();
